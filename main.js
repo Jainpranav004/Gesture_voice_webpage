bf50c73236d7169d88399904768b835d8d9275d9
ABHI-Theq <abhi03085e@gmail.com>
2024-08-21 16:47:25 +0530

    h

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ const app=express()
 const ejs=require('ejs')
 app.use(express.json())
 app.use(express.static(path.join(__dirname,'public')))
-
-
 app.set('view engine','ejs')
 app.get('/home',(req,res,next)=>{
     res.render('index.ejs',{url:req.protocol+"://"+req.headers.host})
