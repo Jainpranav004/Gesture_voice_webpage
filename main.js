a0aa424d668b774614ba7066946da14f3d562351
Pranav jain <jainapranav02@gmail.com>
2024-08-21 17:24:03 +0530

    1

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ const ejs=require('ejs')
 app.use(express.json())
 app.use(express.static(path.join(__dirname,'public')))
 app.set('view engine','ejs')
+
 app.get('/home',(req,res,next)=>{
     res.render('index.ejs',{url:req.protocol+"://"+req.headers.host})
     next()
 })
+
 
 module.exports=app; 
