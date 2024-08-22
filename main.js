480b41dd6d2834aae7c12157d761f11819fae09b
ABHI-Theq <abhi03085e@gmail.com>
2024-08-22 19:42:45 +0530

    frontend commits

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname,'public')))
 app.set('view engine','ejs')
 app.get('/home',(req,res,next)=>{
+    // const logo="public/images/img1.png";
     res.render('index.ejs',{url:req.protocol+"://"+req.headers.host})
     next()
 })
