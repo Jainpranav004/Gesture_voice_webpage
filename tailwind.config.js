db017e54e223012433ab573e388ddd6eeabf8607
Pranav jain <jainapranav02@gmail.com>
2024-08-25 17:35:33 +0530

    Made help page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode :'jit',
   content: ['./views/**/*.ejs'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'wrc' : "#C39B3A",
+      }
+    },
   },
   plugins: [],
 }
