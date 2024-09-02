228c3bfa829e7c356ee5cc5f565f0e33f9f00066
Pranav jain <jainapranav02@gmail.com>
2024-09-02 18:59:17 +0530

    Dataset made

---
+++ models/data.js
@@ -0,0 +1,97 @@
+const module = [
+//Daily usage items
+    {  module_no:"3" ,
+        vid_url : "https://www.youtube.com/watch?v=Qc72veU0a6g",
+        text: "Ball"
+    },
+    {  module_no:"3" ,
+        vid_url : "https://www.youtube.com/watch?v=l2ytof4d1r0",
+        text: "Book"
+    },
+    {  module_no:"3" ,
+        vid_url : "https://www.youtube.com/watch?v=Rty4lN6R8LY",
+        text: "Car"
+    },
+    {  module_no:"3" ,
+        vid_url : "https://www.youtube.com/watch?v=9in2Hz7k60U",
+        text: "House"
+    },
+    {  module_no:"3" ,
+        vid_url : "https://www.youtube.com/watch?v=JabTaO_zQ-M",
+        text: "Key"
+    },
+    {  module_no:"3" ,
+        vid_url : "https://www.youtube.com/watch?v=NZwNyVXvpTo",
+        text: "Money"
+    },
+    {  module_no:"3" ,
+        vid_url : "https://www.youtube.com/watch?v=wIVO3x0WAfk",
+        text: "Paper"
+    },
+    {  module_no:"3" ,
+        vid_url : "https://www.youtube.com/watch?v=0vbtRdb9zTI",
+        text: "Telephone"
+    },
+    {  module_no:"3" ,
+        vid_url : "https://www.youtube.com/watch?v=T3DNMjNynU0",
+        text: "School"
+    },
+
+
+    //Relations and occupations
+    {  module_no:"3" ,
+        vid_url : "https://www.youtube.com/watch?v=rOcFkQzLhLQ&t=9s",
+        text: "Brother"
+    },
+    {  module_no:"3" ,
+        vid_url : "www.youtube.com/watch?v=Gm2HCSz7Jqg",
+        text: "Father"
+    },
+    {  module_no:"3" ,
+        vid_url : "https://www.youtube.com/watch?v=ElXP4DjT3H8",
+        text: "Friends"
+    },
+    {  module_no:"3" ,
+        vid_url : "https://www.youtube.com/watch?v=-5lgXJQG0EM",
+        text: "Doctor"
+    },
+   
+
+    //Unspecified adjectives
+    {  module_no:"3" ,
+        vid_url : "https://www.youtube.com/watch?v=nOfYw4qFT98",
+        text: "Calm"
+    },
+    {  module_no:"3" ,
+        vid_url : "https://www.youtube.com/watch?v=dguva7pmiNk",
+        text: "Hungry"
+    },
+    {  module_no:"3" ,
+        vid_url : "https://www.youtube.com/watch?v=LZreqlssnWQ",
+        text: "Love"
+    },
+    {  module_no:"3" ,
+        vid_url : "https://www.youtube.com/watch?v=fggt2fOzJsk",
+        text: "Pain"
+    },
+    {  module_no:"3" ,
+        vid_url : "https://www.youtube.com/watch?v=-Em79Yvwac4",
+        text: "Think"
+    },
+    {  module_no:"3" ,
+        vid_url : "https://www.youtube.com/watch?v=FEi50jcTxxI",
+        text: "Stop"
+    },
+    {  module_no:"3" ,
+        vid_url : "https://www.youtube.com/watch?v=e4lux9dnPcw",
+        text: "Sleep"
+    },
+    {  module_no:"3" ,
+        vid_url : "https://www.youtube.com/watch?v=heZkGdeEaPE",
+        text: "Pray"
+    },
+
+    
+   
+
+]
